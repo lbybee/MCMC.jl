c07efcc39f5c0a194c52486a1b54bc345fafe10e
lbybee <leland.bybee@gmail.com>
2014-05-11 17:04:42 -0500

    fix bugs in gelman rubin diagnostic

--- src/diagnostics/gelman_rubin.jl
+++ src/diagnostics/gelman_rubin.jl
@@ -1,5 +1,23 @@
 import Base: mean, var
 
+function gen_rhat(traces, m, n)
+
+    var_array = [var(a) for a in traces]
+    mean_array = [mean(a) for a in traces]
+    mean_mean = mean(mean_array)
+
+    W = mean(var_array)
+
+    B = (n * (m - 1)) * sum([(a - mean_mean) for a in mean_array])
+
+    pVar = (1 - 1 / n) * W + (1 / n) * B
+
+    R = sqrt(pVar / W)
+
+    return R
+end
+
+
 function gelman_rubin(traces)
     
     m = size(traces)[1]
@@ -16,6 +34,8 @@ function gelman_rubin(traces)
         n = n_array[1]
     end
 
+    test = [size(a) for a in traces]
+    print(test)
     dim_array = [size(a)[2] for a in traces]
 
     if size(unique(dim_array))[1] != 1
@@ -25,20 +45,8 @@ function gelman_rubin(traces)
     end
 
     if dim > 1
-        return [gelman_rubin([a[:,i] for a in traces]) for i = 1:dim]
+        return [gen_rhat([a[:,i] for a in traces], m, n) for i = 1:dim]
+    else
+        return gen_rhat(traces, m, n)
     end
-
-    var_array = [var(a) for a in traces]
-    mean_array = [mean(a) for a in traces]
-    mean_mean = mean(mean_array)
-
-    W = mean(var_array)
-    
-    B = (n * (m - 1)) * sum([(a - mean_mean) for a in mean_array])
-
-    pVar = (1 - 1 / n) * W + (1 / n) * B
-
-    R = sqrt(pVar / W)
-
-    return R
 end
